5d55a364f3844a43f3e75b12a840822407d0a659
Carmyo <carmelodicristina@gmail.com>
2022-10-10 10:27:23 +0200

    import Header/Main in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
+    <HeaderComponent/>
+    <MainComponent/>
+    
    
   </div>
 </template>
 
 <script>
+import HeaderComponent from './components/HeaderComponent.vue'
+import MainComponent from './components/MainComponent.vue'
 
 
 export default {
   name: 'App',
   components: {
+    HeaderComponent,
+    MainComponent
    
   }
 }
