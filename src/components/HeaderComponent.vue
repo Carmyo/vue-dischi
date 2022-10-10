72cccf762e67bb8941427c86330c878cbb41a0fe
Carmyo <carmelodicristina@gmail.com>
2022-10-10 10:36:04 +0200

    Header done

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>Header</div>
+  <Header>
+    <img src="../assets/logo.png" alt="">
+  </Header>
 </template>
 
 <script>
@@ -10,5 +12,16 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+*{
+    margin: 0 auto;
+}
+header {
+    height: 50px;
+    background-color: hsl(210deg 21% 23%);
+}
+img {
+    height: 50%;      
+    padding: 1% 2%;
+}
 
 </style>
